134f3d94084c1110a0aa540c19de50499f2d5f39
gyro永不抽风 <1247006353@qq.com>
2021-09-20 23:29:05 +0800

    feat: add csharp interface to use .exe

---
+++ Max4Min/Max4Min/Runtime.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace Max4Min
+{
+    public static class Runtime
+    {
+        private static string baseDir = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "../"));
+
+        public static void Hook()
+        {
+            Process.Start(Path.Combine(baseDir, "Max4MinCoreRuntimeHook-x86.exe"));
+            if (Environment.Is64BitOperatingSystem)
+            {
+                Process.Start(Path.Combine(baseDir, "Max4MinCoreRuntimeHook-x64.exe"));
+            }
+        }
+
+        public static void UnHook()
+        {
+            Process.Start(Path.Combine(baseDir, "Max4MinCoreRuntimeUnHook-x86.exe"));
+            Process.Start("taskkill.exe", "/IM Max4MinCoreRuntimeHook-x86.exe /F");
+            Process.Start("taskkill.exe", "/IM Max4MinCoreRuntimeUnHook-x86.exe /F");
+            if (Environment.Is64BitOperatingSystem)
+            {
+                Process.Start(Path.Combine(baseDir, "Max4MinCoreRuntimeUnHook-x64.exe"));
+                Process.Start("taskkill.exe", "/IM Max4MinCoreRuntimeHook-x64.exe /F");
+                Process.Start("taskkill.exe", "/IM Max4MinCoreRuntimeUnHook-x64.exe /F");
+            }
+        }
+    }
+}
